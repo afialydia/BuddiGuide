fb8dfcee2640fabea8b1b2b5ce80dd6a4e065c11
Afia-Lydia <you@example.com>
2020-04-21 17:45:51 -0400

    test

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const cors = require("cors");
 const helmet = require("helmet");
 
 const server = express();
+require("dotenv").config();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-server.use(express.static(path.join(__dirname, "client/build")));
 
 const strainRouter = require("./api/routes/strain_router");
 const userRouter = require("./api/routes/user_router");
@@ -21,9 +21,12 @@ server.get("/", (req, res) => {
 	res.status(200).json({ status: "I'm alive and on fire :fireball:" });
 });
 
-
-server.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
+if (process.env.NODE_ENV === 'production') {
+	// Set static   
+	server.use(express.static('client/build'));
+	server.get('*', (req, res) => {
+	  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+  }
 
 module.exports = server;
