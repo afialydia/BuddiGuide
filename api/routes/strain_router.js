3fbe7eed44e107aedcecb7f3ee05dcb5ca8ddce9
Afia-Lydia <you@example.com>
2020-04-23 12:20:58 -0400

    updated error handling

--- api/routes/strain_router.js
+++ api/routes/strain_router.js
@@ -6,7 +6,7 @@ router.get("/", (req, res) => {
 	Strains.findAllStrains()
 		.then((strainData) => res.status(200).json(strainData))
 		.catch((err) =>
-			res.status(500).json({ message: "Looks like something broke", err })
+			res.status(500).json({ message: `Looks like something broke ${err}`  })
 		);
 });
 
